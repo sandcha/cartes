bdd5bf5d7bbf41e9380b1fc50d37948642eec4a7
sandcha <sandcha@users.noreply.github.com>
2020-07-25 18:25:18 +0200

    Supprime du code inutile

--- tunisie.py
+++ tunisie.py
@@ -1,12 +1,8 @@
+import pandas
 import geopandas as gp
 import matplotlib.pyplot as plt
 
 
 geojson = gp.read_file('./delegations-full.geojson')
-
-print("Degrés", geojson.length)
-deg2km = 111  # https://ocefpaf.github.io/python4oceanographers/blog/2015/03/30/geo_pandas/
-print("Kilomètres", geojson.length * deg2km)
-
 geojson.plot()
 plt.show()
